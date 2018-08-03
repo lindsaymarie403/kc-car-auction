83501c17bbba61917a6714d8ed1334499edc6f34
Lindsay Huth <huthlindsay@gmail.com>
2018-08-02 19:03:36 -0500

    added right graph

--- ageofcars.R
+++ ageofcars.R
@@ -8,4 +8,5 @@ library(ggplot2)
 # make density plot
 den1 <- ggplot(cars, aes(cars$Year)) +
   geom_density()
-den1
+den1 + theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+           panel.background = element_blank(), axis.line = element_line(colour = "black"))
